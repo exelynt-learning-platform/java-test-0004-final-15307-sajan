ac594bb90882f02a107287f82b7d0dc37c85885b
SAJAN  AADE <143798451+git-saju@users.noreply.github.com>
2026-03-14 15:04:56 +0530

    Create ConcentricNumberSquare.java

---
+++ ConcentricNumberSquare.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ConcentricNumberSquare {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of n: ");
+        int n = sc.nextInt();
+
+        int size = 2 * n - 1;
+
+        for (int i = 0; i < size; i++) {
+
+            for (int j = 0; j < size; j++) {
+
+                int top = i;
+                int left = j;
+                int bottom = size - 1 - i;
+                int right = size - 1 - j;
+
+                int min = Math.min(Math.min(top, bottom), Math.min(left, right));
+
+                System.out.print((n - min) + " ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
